82d8cd5e898e92bc4a371d1edf80b58bd1627f3b
Alfonso <81327113+foncheto@users.noreply.github.com>
2024-11-27 13:23:23 -0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,12 +23,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-  })
-);
+app.use(cors());
 
 /* ROUTES */
 app.get("/", (req, res) => {
